fd40e3b87c8e6fa5974d15c5c4fe5783900b6ebb
Guy Sopher <guysopher@gmail.com>
2013-03-08 09:21:17 +0200

    changed full title thing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "Sssssopher!"
+    base_title and return if page_title.empty?
+    "#{base_title} | #{page_title}"
+  end
 end
