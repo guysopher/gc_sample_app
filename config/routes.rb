483231ad609071001f70db387088929d61c34065
Guy Sopher <guysopher@gmail.com>
2013-03-15 12:29:13 +0200

    signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
-  resources :users
-
   root to: 'static_pages#home'
 
+  resources :users
   match '/signup',  to: 'users#new'
+
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new'
+  match '/signout',  to: 'sessions#destroy', via: delete
 
   match '/home',    to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
